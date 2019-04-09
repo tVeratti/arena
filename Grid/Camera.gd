cd983a93785ed862c1e82a37639b1534c5d02bfe
Tatiana Veratti <tatianaveratti@gmail.com>
2019-04-08 19:41:32 -0500

    allow manual camera movement

--- Grid/Camera.gd
+++ Grid/Camera.gd
@@ -3,7 +3,8 @@ extends Camera2D
 # Target
 onready var _target:Vector2 = position
 var _speed:float = 5.0
-var _tolerance:float = 0.2
+var _manual_speed:float = 10.0
+var _tolerance:float = 0.01
 
 
 # Zoom
@@ -14,6 +15,8 @@ var _zoom_tolerance:float = 0.1
 
 
 func _process(delta):
+    _manual_camera_input()
+    
     # Move the camera to the target tile (x, y)
     var distance = position.distance_to(_target)
     if distance >= _tolerance:
@@ -35,6 +38,18 @@ func _input(event):
             _zoom_target = zoom - _zoom_step
         elif event.button_index == BUTTON_WHEEL_DOWN:
             _zoom_target = zoom + _zoom_step
+
+       
+func _manual_camera_input():
+    # Manual camera _target movement
+    if Input.is_action_pressed("ui_up"):
+        _target.y -= _manual_speed
+    if Input.is_action_pressed("ui_down"):
+        _target.y += _manual_speed
+    if Input.is_action_pressed("ui_right"):
+        _target.x += _manual_speed
+    if Input.is_action_pressed("ui_left"):
+        _target.x -= _manual_speed
 
 
 func set_target(target):
