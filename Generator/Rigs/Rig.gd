fe29d718cfc2b819756dd2cc91a867982b3eb3af
Tatiana <tatianaveratti@gmail.com>
2019-05-04 01:03:15 -0500

    experiment with rigs, anim

--- Generator/Rigs/Rig.gd
+++ Generator/Rigs/Rig.gd
@@ -24,8 +24,19 @@ func set_textures(textures:Dictionary):
     
 
 func set_facing_textures(parts):
+    var region_padding = 100
     for part in parts.get_children():
-        part.texture = textures[part.name]
+        if part is Sprite:
+            var rect = part.region_rect
+            part.region_rect = Rect2(\
+                rect.position.x - region_padding, \
+                rect.position.y - region_padding, \
+                rect.size.x + region_padding, \
+                rect.size.y + region_padding)
+            part.texture = textures[part.name]
+            
+            if part.get_child_count() > 0:
+                set_facing_textures(part)
 
 
 func set_facing(facing:String):
